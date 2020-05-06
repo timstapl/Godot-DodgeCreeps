c346f37032fcb9600821d8d1a5a32e131e0f887b
Timothy Stapleton <retrocede@outlook.com>
2020-05-05 22:01:00 -0700

    clear out any old mobs when game start

--- Main.gd
+++ Main.gd
@@ -48,3 +48,4 @@ func _on_MobTimer_timeout():
 	# Set the mobs velocity
 	mob.linear_velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
 	mob.linear_velocity = mob.linear_velocity.rotated(direction)
+	$HUD.connect("start_game", mob, "_on_start_game")
